97e5d71a0771325cacf165277c029ed1dda21b5b
Grzegorz <grafi71@o2.pl>
2020-11-28 09:57:09 +0100

    Add commans

--- src/pzem/pzem.py
+++ src/pzem/pzem.py
@@ -15,6 +15,9 @@ TIMEOUT = 5 # 5 seconds serial timeout
 XONXOFF = 0
 RTSCTS = 0
 
+CMD_RESET = [0xf8,0x42,0xc2,0x41]
+CMD_QUERY = [0xf8,0x04,0x00,0x00,0x00,0x0a,0x64,0x64]
+
 log = logging.getLogger(__name__)
 
 class PZEM:
@@ -36,7 +39,11 @@ class PZEM:
         '''
         Query device and log data to csv file
         '''
+        queries_time = self.cfg.queries_time
+        queries_number = self.cfg.queries_number
         writer = csv.writer(self.cfg.outfile, quoting=csv.QUOTE_MINIMAL)
+        while True:
+            pass
         self.cfg.outfile.close()
 
     def reset(self):
